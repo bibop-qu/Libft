b50e5fb0965e79a87052e1618ea57b8110bd1011
Bastien LE-QUERE-DUMONT <basle-qu@e2r3p6.42.fr>
2015-01-14 19:30:33 +0100

    modif

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,50 +6,13 @@
 /*   By: basle-qu <basle-qu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 15:52:22 by basle-qu          #+#    #+#             */
-/*   Updated: 2015/01/02 16:46:44 by basle-qu         ###   ########.fr       */
+/*   Updated: 2015/01/14 19:29:14 by basle-qu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdlib.h>
-
-static size_t		ft_strlen(const char *s)
-{
-	size_t		len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
-static void			*ft_memmove(void *dst, const void *src, size_t len)
-{
-	size_t				i;
-	unsigned char		*ptrdst;
-	const unsigned char	*ptrsrc;
-
-	i = 0;
-	ptrdst = dst;
-	ptrsrc = src;
-	if (len <= ft_strlen(dst))
-	{
-		ptrdst[i] = ptrsrc[i];
-		i++;
-		len--;
-	}
-	else
-	{
-		dst = (char*)malloc(sizeof(char) * len + 1);
-		while (len)
-		{
-			ptrdst[i] = ptrsrc[i];
-			i++;
-			len--;
-		}
-	}
-	return (dst);
-}
+#include "libft.h"
 
 void				*ft_memcpy(void *dst, const void *src, size_t n)
 {
